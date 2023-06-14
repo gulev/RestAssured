c2740d38810cdd0b406c4d504bdee577a296d61e
svetlin.gulev <svetlin.gulev@primeholding.com>
2023-06-14 22:44:56 +0300

    proccessing response

--- src/test/java/com/rest/Test.java
+++ src/test/java/com/rest/Test.java
@@ -1,5 +1,8 @@
 package com.rest;
 
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
@@ -10,7 +13,7 @@ public class Test {
     public void validate_get_status_code(){
         given().
                 baseUri("https://api.postman.com").
-                header("X-Api-Key","PMAK-645c1951c8092459ed5c6ac3-5f1328f28379a3b5aa9f78bd95f5915ec1").
+                header("X-Api-Key","PMAK-6484d8fa58115f25366d9653-5cb2599c32b0c5f0c889823ee47aa8bcf8").
         when().
                     get("/workspaces").
         then().
@@ -30,26 +33,51 @@ public class Test {
                 log().all().
                 assertThat().
                 statusCode(200).
-                body("workspaces.name",hasItems("My Workspace","gulev"),
-                "workspaces.type",hasItems("personal","personal"),
-                        "workspaces[0].name",equalTo("My Workspace"),
-                        "workspaces[0].name",is(equalTo("My Workspace")),
-                        "workspaces.size()",equalTo(2),
-                        "workspace.name",hasItem("gulev")
+                body("workspaces.name", hasItems("My Workspace","gulev"),
+                "workspaces.type", hasItems("personal","personal"),
+                        "workspaces[0].name", equalTo("My Workspace"),
+                        "workspaces[0].name", is(equalTo("My Workspace")),
+                        "workspaces.size()", equalTo(2),
+                        "workspaces.name", hasItem("gulev")
                 );
 
     }
 
+    @org.testng.annotations.Test
     public void extract_response(){
-        given().
+        Response res = given().
                 baseUri("https://api.postman.com").
-                header("X-Api-Key","PMAK-645c1951c8092459ed5c6ac3-5f1328f28379a3b5aa9f78bd95f5915ec1").
+                header("X-Api-Key","PMAK-6484d8fa58115f25366d9653-5cb2599c32b0c5f0c889823ee47aa8bcf8").
         when().
                 get("/workspaces").
         then().
                 log().all().
                 assertThat().
-                statusCode(200);
+                statusCode(200).
+                extract().
+                response();
+
+        System.out.println("response: " + res.asString());
+    }
+
+
+    @org.testng.annotations.Test
+    public void extract_single_value_from_response(){
+        String name = given().
+                baseUri("https://api.postman.com").
+                header("X-Api-Key","PMAK-6484d8fa58115f25366d9653-5cb2599c32b0c5f0c889823ee47aa8bcf8").
+                when().
+                get("/workspaces").
+                then().
+                log().all().
+                assertThat().
+                statusCode(200).
+                extract().
+                response().path("workspaces[0].name");
+        System.out.println("workspace name: " + name);
+        //System.out.println("workspace name: " + JsonPath.from(res).getString("workspaces[0].name"));
+        //System.out.println("workspace name: " + jsonPath.getString("workspaces[0].name"));
+        //System.out.println("workspace name: " + res.path("workspaces[0].name"));
 
     }
 }
