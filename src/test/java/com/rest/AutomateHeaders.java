5c594b37d35f13eeaad67e0bf43edf0051d61c2d
svetlin.gulev <svetlin.gulev@primeholding.com>
2023-06-19 16:13:00 +0300

    clearing up some dependencies

--- src/test/java/com/rest/AutomateHeaders.java
+++ src/test/java/com/rest/AutomateHeaders.java
@@ -1,11 +1,5 @@
 package com.rest;
 
-import io.restassured.config.LogConfig;
-import org.apache.http.Header;
-
-import java.util.HashMap;
-
-import static io.restassured.RestAssured.config;
 import static io.restassured.RestAssured.given;
 
 public class AutomateHeaders {
@@ -44,9 +38,9 @@ public class AutomateHeaders {
                 baseUri("https://a481d962-4054-4a8a-9c69-3e71848369d4.mock.pstmn.io/").
                 header("header", "value1").
                 header("x-mock-match-request-headers", "header").
-                when().
+        when().
                 get("/get").
-                then().
+        then().
                 log().all().
                 assertThat().statusCode(200).
                 extract().
