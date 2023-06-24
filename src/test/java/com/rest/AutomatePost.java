95e170fcaf55c667c7b681729ab3c779a1cc3a0a
svetlin.gulev <svetlin.gulev@primeholding.com>
2023-06-25 02:23:24 +0300

    automate post with bdd

---
+++ src/test/java/com/rest/AutomatePost.java
@@ -0,0 +1,49 @@
+package com.rest;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeClass;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.matchesPattern;
+
+public class AutomatePost {
+
+    @BeforeClass
+    public void beforeClass(){
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.setBaseUri("https://api.postman.com");
+        requestSpecBuilder.addHeader("X-Api-Key",
+                "PMAK-6484d8fa58115f25366d9653-5cb2599c32b0c5f0c889823ee47aa8bcf8");
+        requestSpecBuilder.setContentType(ContentType.JSON);
+        RestAssured.requestSpecification = requestSpecBuilder.build();
+
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder().
+                expectStatusCode(200).
+                expectContentType(ContentType.JSON).
+                log(LogDetail.ALL);
+        RestAssured.responseSpecification = responseSpecBuilder.build();
+    }
+
+    @org.testng.annotations.Test
+    public void validate_post_request_bdd_style() {
+        String payload = "{\n" +
+                "    \"workspace\": {\n" +
+                "        \"name\": \"myFirstWorkspace1\",\n" +
+                "        \"type\": \"personal\",\n" +
+                "        \"description\": \"Rest Assured created this\"\n" +
+                "    }\n" +
+                "}";
+        given().
+                body(payload).
+        when().
+                post("/workspaces").
+        then().assertThat().body("workspace.name",equalTo("myFirstWorkspace1"));
+    }
+}
