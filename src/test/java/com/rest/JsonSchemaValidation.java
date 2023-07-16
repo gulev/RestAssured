c30d15642bb0cc94123f1d03a39326528c34465b
svetlin.gulev <svetlin.gulev@primeholding.com>
2023-07-17 01:12:22 +0300

    json schema validation test

---
+++ src/test/java/com/rest/JsonSchemaValidation.java
@@ -0,0 +1,22 @@
+package com.rest;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class JsonSchemaValidation {
+    @org.testng.annotations.Test
+    public void validateJsonSchema(){
+        given().
+                baseUri("https://postman-echo.com").
+                log().all().
+        when().
+                get("/get").
+        then().
+                log().all().
+                assertThat().
+                statusCode(200).body(matchesJsonSchemaInClasspath("EchoGet.json"));
+
+    }
+}
